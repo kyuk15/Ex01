e7ce762e03a2ddd7369bf51c66c48a54eafba424
kyuk15 <s13000008@u-aizu.ac.jp>
2023-06-13 11:49:39 +0900

    Dice game: initial revision

---
+++ DiceGameApplication.java
@@ -0,0 +1,14 @@
+class DiceGameApplication{
+    public static void main(String[] args){
+	int num1,num2,sum;
+	
+	System.out.println("Rolling dice...");
+	num1=(int)(Math.random()*5)+1;
+	num2=(int)(Math.random()*5)+1;
+	sum=num1+num2;
+
+	System.out.println("Die 1: "+num1);
+	System.out.println("Die 2: "+num2);
+	System.out.println("Total value: "+sum);
+    }
+}
